9ef2eeab3dbe8610b7efd38689167ba7fc77c039
Kamal <madara11rinne@gmail.com>
2023-04-08 11:03:56 -0700

    compare

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,17 +14,13 @@ int wildcmp(char *s1, char *s2)
 
 		if (*s2 == '*')
 			return (wildcmp(s1, s2 + 1));
-		return (0);
+		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
 	{
-	if (wildcmp(s1, s2 + 1))
-		return (1);
-	if (*s1 != '\0' && wildcmp(s2, s1 + 1))
-		return (1);
-	return (0);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	else if (*s1 != *s2)
-		return (0);
-	return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
