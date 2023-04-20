aff41fa400ad4f9a3e6800310e1c5663e1d2460c
Kamal <madara11rinne@gmail.com>
2023-04-20 11:11:32 -0700

    a lot of functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 /**
  * main - check
  * @argc: num
@@ -9,4 +9,17 @@ int main(int argc, char **argv)
 {
 	int (*fc)(int, int), a, b;
 
-	if
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	fc = get_op_func(argv[2]);
+	if (!fc)
+		printf("Error\n"), exit(99);
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", fc(a, b));
+	return (0);
+}
