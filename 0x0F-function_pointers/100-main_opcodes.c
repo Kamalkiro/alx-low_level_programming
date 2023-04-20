4d27e86708b49270e6c065e50ea8f22e9d19221a
Kamal <madara11rinne@gmail.com>
2023-04-20 10:49:09 -0700

    mul

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,18 +8,25 @@
  */
 int main(int argc, char *argv[])
 {
-	int num;
-	char *fp = (char *)main;
+	int num, x;
+	char *fp;
 
 	num = atoi(argv[1]);
 	if (argc < 2 || argc > 2 || num < 0)
-		printf("Error'\n'");
+		printf("Error\n");
 	if (argc < 2 || argc > 2)
 		exit(1);
 	if (num < 0)
 		exit(2);
 
-	while (num--)
-		printf("%02hhx%s", *fp++, num ? " " : "\n");
-	return (0);
+	fp = (char *)main;
+
+	for (x = 0; x < num; x++)
+	{
+		if (x == num - 1)
+		{
+			printf("%02hhx\n", fp[x]);
+			break;
+		}
+	}
 }
