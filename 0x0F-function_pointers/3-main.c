e4302b208a88e4a1180b52e8937511d4cd1c61c6
Kamal <madara11rinne@gmail.com>
2023-04-19 20:00:27 -0700

    late

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "calc.h"
+/**
+ * main - check
+ * @argc: num
+ * @argv: arg
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int (*fc)(int, int), a, b;
+
+	if
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ * op_add - adds
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subs
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divise
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
