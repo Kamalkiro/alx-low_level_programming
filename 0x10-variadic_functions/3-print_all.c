81ada7500d29d235c8c4dc9fac7ed516ea44a8e4
Kamal <madara11rinne@gmail.com>
2023-04-20 17:42:55 -0700

    print it

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * charp - printing char
+ * @arg: argument
+ */
+void charp(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * intp - printing integer
+ * @arg: argument
+ */
+void intp(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * floatp - printing float
+ * @arg: argument
+ */
+void floatp(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * stringp - printing string
+ * @arg: argument
+ */
+void stringp(va_list arg)
+{
+	char *string;
+
+	string = va_arg(arg, char *);
+
+	if (!string)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
+/**
+ * print_all - printing anything
+ * @format: array
+ */
+void print_all(const char * const format, ...)
+{
+	char *sep = "";
+	int x = 0, y = 0;
+	va_list arg;
+
+	variable choice[] = {
+		{'c', charp},
+		{'i', intp},
+		{'f', floatp},
+		{'s', stringp},
+		{'\0', '\0'}
+	};
+
+	va_start(arg, format);
+	while (format && format[y])
+	{
+		while (choice[x].ch != '\0')
+		{
+			if  (choice[x].ch == format[y])
+			{
+				printf("%s%s", sep, ", ");
+				choice[x].point(arg);
+				sep = "";
+			}
+			x++;
+		}
+		y++;
+	}
+	va_end(arg);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct var - variable
+ * @ch: choice
+ * @point: pointer
+ */
+typedef struct var
+{
+	char ch;
+	void (*point)(va_list);
+} variable;
 #endif
