adf615524da55ed4ff06c7ea41c13e95a12ff11b
Kamalkiro <madara11rinne@gmail.com>
2023-04-27 19:14:19 -0700

    list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
  * _putchar - puts characters
- * Return: 0
  */
-void __attribute__ ((constructor)) execute_before_main()
+void __attribute__ ((constructor)) _putchar()
 {
-	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
