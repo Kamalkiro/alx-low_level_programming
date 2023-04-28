a6dbc281476aea0055746fe4e612df90d2c29d26
Kamalkiro <madara11rinne@gmail.com>
2023-04-27 19:03:11 -0700

    0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * _putchar - puts characters
+ * Return: 0
+ */
+int _putchar(void)
+{
+        printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+        return (0);
+}
