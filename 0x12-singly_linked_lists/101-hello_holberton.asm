792288a0f306b5fe54a4b9429cbdd95c8a36c977
Kamalkiro <madara11rinne@gmail.com>
2023-04-27 19:55:38 -0700

    list

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+section		.text
+	extern	printf		;Call c function from external lib
+	global	main		;Main is explained below
+main:
+	mov		rdi, Hello	
+	mov		rax, 0
+	call	printf		;printf called from c lib
+	ret			;Or return
+section		.data
+	Hello db "Hello, Holberton", 0xa, 0	;printed by printf function
