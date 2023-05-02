cb6bb29fc3fabfad612b914b96b933a66d8ccbfc
Kamalkiro <madara11rinne@gmail.com>
2023-05-02 13:03:46 -0700

    all tasks done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *n = head;
 	unsigned int i = 0;
 
-	while (i && i < index)
-	{
-		n = n->next;
-		i++;
-	}
+	if (head == NULL)
+		return (NULL);
+	for (n = head, i = 0; n && i < index; n = n->next, i++)
+		;
+	if (i == index)
+		return (head);
 	return (n);
 }
