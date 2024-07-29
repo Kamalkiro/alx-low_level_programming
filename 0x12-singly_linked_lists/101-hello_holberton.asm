d202372c0814a07bfc5d35747c22a193dba6a936
Kamalkiro <madara11rinne@gmail.com>
2024-07-29 21:34:36 +0100

    fixing the return issue

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,9 +2,10 @@ section		.text
 	extern	printf		;Call c function from external lib
 	global	main		;Main is explained below
 main:
-	mov		rdi, Hello
-	mov		rax, 0
+	mov		rdi, Hello	;Moves the string into the register responsible for storing
+	mov		rax, 0		;set the 
 	call	printf		;printf called from c lib
+	mov		rax, 0		;return 0
 	ret			;Or return
 section		.data
 	Hello db "Hello, Holberton", 0xa, 0	;printed by printf function
