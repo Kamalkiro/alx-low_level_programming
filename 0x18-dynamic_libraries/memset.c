95db27942040f99dc3b0be49ef43813e83d80861
Kamalkiro <madara11rinne@gmail.com>
2024-11-29 21:02:20 +0100

    learned to handle dynamic libraries

---
+++ 0x18-dynamic_libraries/memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with value
+ * @s:starting address
+ * @b:value desired
+ * @n:number of bytes
+ * Return:array with new values
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
